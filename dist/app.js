a1591bdc9de8a8cb5001ad9114a00617e3fe08d4
hieutct <hieutct@solazu.com>
2018-01-17 15:49:31 +0700

    commit

--- dist/app.js
+++ dist/app.js
@@ -8,53 +8,55 @@ app.listen(80, function () {
     console.log('connected to *:80');
 });
 function handler(req, res) {
-    var cookies = new Cookies(req, res);
-    var filePath = '.' + req.url;
-    if (filePath == './') {
-        cookies.set('userId', random_name(), { httpOnly: false });
-        filePath = './index.html';
-    }
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
-    fs.readFile(filePath, function (error, content) {
-        if (error) {
-            if (error.code == 'ENOENT') {
-                fs.readFile('./404.html', function (error, content) {
-                    res.writeHead(200, { 'Content-Type': contentType });
-                    res.end(content, 'utf-8');
-                });
-            }
-            else {
-                res.writeHead(500);
-                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-                res.end();
-            }
-        }
-        else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
+    // var cookies = new Cookies(req, res);
+    // var filePath = '.' + req.url;
+    // if (filePath == './') {
+    //     cookies.set('userId', random_name(), { httpOnly: false });
+    //     filePath = './index.html';
+    // }
+    // var extname = path.extname(filePath);
+    // var contentType = 'text/html';
+    // switch (extname) {
+    //     case '.js':
+    //         contentType = 'text/javascript';
+    //         break;
+    //     case '.css':
+    //         contentType = 'text/css';
+    //         break;
+    //     case '.json':
+    //         contentType = 'application/json';
+    //         break;
+    //     case '.png':
+    //         contentType = 'image/png';
+    //         break;
+    //     case '.jpg':
+    //         contentType = 'image/jpg';
+    //         break;
+    //     case '.wav':
+    //         contentType = 'audio/wav';
+    //         break;
+    // }
+    // fs.readFile(filePath, function (error, content) {
+    //     if (error) {
+    //         if (error.code == 'ENOENT') {
+    //             fs.readFile('./404.html', function (error, content) {
+    //                 res.writeHead(200, { 'Content-Type': contentType });
+    //                 res.end(content, 'utf-8');
+    //             });
+    //         }
+    //         else {
+    //             res.writeHead(500);
+    //             res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+    //             res.end();
+    //         }
+    //     }
+    //     else {
+    //         res.writeHead(200, { 'Content-Type': contentType });
+    //         res.end(content, 'utf-8');
+    //     }
+    // });
+
+    res.end('hello word');
 }
 io.on('connection', function (socket) {
     socket.on('chatSend', function (data) {
