7daecfd6144e725432cba647874a0d4804b8cedd
hieutct <hieutct@solazu.com>
2018-01-17 16:22:15 +0700

    fix

--- dist/statics/client.js
+++ dist/statics/client.js
@@ -1,5 +1,5 @@
 (function ($) {
-    var socket = io.connect('http://localhost');
+    var socket = io.connect();
     var _userId = getCookie('userId');
 
     socket.on('chatReceive', function (data) {
